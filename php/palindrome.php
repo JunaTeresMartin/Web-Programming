b602a514fe6360c864b6743c7abd99eee5ce404b
JunaTeresMartin <83876430+JunaTeresMartin@users.noreply.github.com>
2023-11-03 11:14:51 +0530

    Update palindrome.php

--- php/palindrome.php
+++ php/palindrome.php
@@ -6,8 +6,15 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="palindrome.php" method="post">
+    <p>Enter a number: <input type="number" name='mynum'></p>
+    <input type="submit" value="Check Palindrome" name='myButton'/>
+    </form>
     <?php
-    $n=(int)readline("Input a number");
+    if(isset($_POST['myButton']))
+    {
+        $n = $_POST['mynum']; //take the value
+    }
     $x=0;
     $num=$n;
     while(floor($n)){
@@ -16,7 +23,10 @@
         $n=$n/10;
     }
     if($num==$x){
-        echo 
+        echo "<br/>$num is a palindrome";
+    }
+    else{
+        echo "<br/>$num is not a palindrome";
     }
     ?>
 </body>
